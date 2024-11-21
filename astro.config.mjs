810f56d1cfdcd3b6e19558a2045dc76814ccf66a
alex <ochanax@eaglehubs.com>
2024-11-21 13:42:26 -0600

    Agregar configuración inicial de Astro con soporte para múltiples idiomas y tema oscuro

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    },
+    fallback:{
+      en: 'es'
+    } 
+  }
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter',
+  },
+  es: {
+    'nav.home': 'Inicio',
+    'nav.about': 'Acerca de',
+    'nav.twitter': 'Twitter',
+  },
+};
+
+export const showDefaultLang = false;
