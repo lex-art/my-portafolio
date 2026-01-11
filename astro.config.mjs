60e3c45d4f34d05aa6b465e7369557ee2617bf8e
alex <ochanax@eaglehubs.com>
2026-01-10 21:21:38 -0600

    feat: add GitHub links and icons to About and Home sections in both English and Spanish chore: update PDF files for Oscar Chanax's CV

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  prefetch: false,
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
